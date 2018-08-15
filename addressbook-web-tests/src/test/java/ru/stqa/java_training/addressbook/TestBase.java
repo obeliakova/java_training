a3d631b08899b281a44e5c4eaa958517785d4e08
Olga Beliakova <olga_fluffy@mail.ru>
2018-08-16 02:14:56 +0300

    Из тестового класса ContactCreationTests перенесены общие методы в класс TestBase, лишнее удалено

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/TestBase.java
@@ -79,4 +79,34 @@ public class TestBase {
   protected void selectGroup() {
     wd.findElement(By.name("selected[]")).click();
   }
+
+  protected void gotoHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  protected void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  protected void gotoAddNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
 }

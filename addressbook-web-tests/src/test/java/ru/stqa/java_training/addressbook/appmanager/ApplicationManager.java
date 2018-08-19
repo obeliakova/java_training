aa0a76edbb7757f65230e5f32560530b467a2b9f
Olga Beliakova <olga_fluffy@mail.ru>
2018-08-20 02:32:27 +0300

    Заменено сравнение с помощью "==" на сравнение с помощью equals, добавлен новый запускаемый класс в sandbox для сравнения двух этих методов сравнения строк.

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/appmanager/ApplicationManager.java
@@ -26,13 +26,13 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser == BrowserType.FIREFOX) {
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser == BrowserType.CHROME) {
+    } else if (browser.equals(BrowserType.CHROME)) {
       //File fileChromeDriver = new File("C:/Tools/chromedriver.exe");
       //System.setProperty("webdriver.chrome.driver", fileChromeDriver.getAbsolutePath());
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.IE) {
+    } else if (browser.equals(BrowserType.IE)) {
       //File fileIEDriver = new File("C:/Tools/IEDriverServer.exe");
       //System.setProperty("webdriver.ie.driver", fileIEDriver.getAbsolutePath());
       wd = new InternetExplorerDriver();

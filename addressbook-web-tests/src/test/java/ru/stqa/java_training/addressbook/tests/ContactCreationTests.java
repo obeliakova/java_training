3053b5d9162583244b24fd66faa431ed6c65b344
Olga Beliakova <olga_fluffy@mail.ru>
2018-09-03 01:12:22 +0300

    В тестах создания группы и контакта теперь используются лямбда-выражения для поиска максимального элемента в списке.

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/ContactCreationTests.java
@@ -20,13 +20,7 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData contactData : after) {
-      if(contactData.getId() > max) {
-        max = contactData.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }

9d315b6f910ab84c0a727453d0f068f463107485
Olga Beliakova <olga_fluffy@mail.ru>
2018-09-09 11:53:04 +0300

    Изменен класс TestBase, теперь методы этого класса вызываются в начале и в конце suite, переменная app стала глобальной.

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/TestBase.java
@@ -1,20 +1,20 @@
 package ru.stqa.java_training.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.java_training.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
